eeb553e0aa02096ee26cdbb5e3c20de01869bfb3
Julien Chaumond <chaumond@gmail.com>
2017-01-30 22:20:15 +0100

    scotus: Supreme Court conversation data

--- chatbot/textdata.py
+++ chatbot/textdata.py
@@ -27,6 +27,7 @@ import random
 
 from chatbot.cornelldata import CornellData
 from chatbot.opensubsdata import OpensubsData
+from chatbot.scotusdata import ScotusData
 
 class Batch:
     """Struct containing batches info
@@ -220,6 +221,9 @@ class TextData:
             elif self.args.corpus == 'opensubs':
                 opensubsData = OpensubsData(self.corpusDir)
                 self.createCorpus(opensubsData.getConversations())
+            elif self.args.corpus == 'scotus':
+                scotusData = ScotusData(self.corpusDir)
+                self.createCorpus(scotusData.getConversations())
 
             # Saving
             print('Saving dataset...')
---
+++ data/scotus/README.md
@@ -0,0 +1,5 @@
+Download and extract the scotus dataset here:
+
+```bash
+wget https://github.com/pender/chatbot-rnn/raw/master/data/scotus/scotus.bz2 && bzip2 -dk scotus.bz2
+```
